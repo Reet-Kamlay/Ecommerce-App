3956c06f877e8663301aa8bc5d052ac2e97a2f6d
Reet Kamlay <santikamlay123@gmail.com>
2025-06-27 04:36:48 +0530

    ninth commit

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(cors({
     }
   },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization","token"],
   credentials: true
 }));
 
