f89a46e38af750a92bd55640f9a1413bbcec8da7
Reet Kamlay <santikamlay123@gmail.com>
2025-06-27 04:27:14 +0530

    seventh commit

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ connectCloudinary();
 
 // ✅ Allow specific frontend domains
 const allowedOrigins = [
-    "https://forever-frontend-mocha-five.vercel.app",
+    "https://forever-frontend-orcin.vercel.app",
 ];
 
 app.use(cors({
